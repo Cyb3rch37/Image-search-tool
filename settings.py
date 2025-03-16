a206280f275f086a952edfb9b9049681b2c7da4a
Cyb3rch37 <gitlab.frame027@passinbox.com>
2025-03-16 20:02:57 +0300

    update on settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import os
+
 ## settings.py (Email & Logging Configuration)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -7,6 +9,19 @@ EMAIL_HOST_USER = 'your-email@gmail.com'
 EMAIL_HOST_PASSWORD = 'your-email-password'
 DEFAULT_FROM_EMAIL = 'your-email@gmail.com'
 ADMIN_EMAIL = 'admin-email@gmail.com'
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),  # This should match the service name in docker-compose.yml
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
 
 LOGGING = {
     'version': 1,
